c874b3c14ee35fec25eb2ac10afae8037b16832a
Ye HtetAung <yehtetaung7655@gmail.com>
2026-08-25 09:20:19 +0630

    Increase wait time from 700ms to 3500ms

--- src/index.js
+++ src/index.js
@@ -1872,7 +1872,7 @@ for(var i=0;i<3;i++){
   void el.offsetWidth;
   el.classList.add("jump-before-change");
 
-  await waitMs(700);
+  await waitMs(3500);
   if(myToken !== mainResultAnimationToken) return;
 }
 
